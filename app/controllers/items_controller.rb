90a9018ea0a4de6f03abbd629ff77c0f113f9b75
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-22 16:52:55 +0900

    先の修正箇所をコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,22 +2,22 @@ class ItemsController < ApplicationController
   @items = Item.order('created_at DESC')
 
   def new
-    @item = Item.new
+    # @item = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect to root_path
-    else
-      remder :new, status: unprocessable_entity
-    end
+    # @item = Item.new(item_params)
+    # if @item.save
+    #   redirect to root_path
+    # else
+    #   remder :new, status: unprocessable_entity
+    # end
   end
 
-  private
+  # private
 
-  def item_oarams
-    params.require(:item).permit(:name, :info, :genre_id)
-  end
+  # def item_oarams
+  #   params.require(:item).permit(:name, :info, :genre_id)
+  # end
 
 end
