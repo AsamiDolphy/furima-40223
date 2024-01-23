5dbdf15543991d31cef40a11c0128b3a5ad3c480
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-23 19:57:16 +0900

    エラーメッセージ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
   # def show
-    # @user = User.find(params[:id])
+  #   user_id = params[:id]
+  #   @user = User.find(user_id)
+  # end
+
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     redirect_to root_path
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
   # end
 end
