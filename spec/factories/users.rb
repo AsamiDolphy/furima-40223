4da194a8fffc58963f6d1aff64ed6f2d44f87857
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-23 20:01:00 +0900

    カナ表記のFactoryBotもFaker生成に変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,8 +6,8 @@ FactoryBot.define do
     password_confirmation { password }
     last_name             { Faker::Name.last_name }
     first_name            { Faker::Name.first_name }
-    last_name_kana        { 'ラストネーム' }
-    first_name_kana       { 'ファーストネーム' }
+    last_name_kana        { Faker::Base.regexify(/[ア-ン゛゜ァ-ォャ-ョーa-zA-Z0-9]{1,20}/) }
+    first_name_kana       { Faker::Base.regexify(/[ア-ン゛゜ァ-ォャ-ョーa-zA-Z0-9]{1,20}/) }
     birth_date            { '2000-01-01' }
   end
 end
