b88d79d30eec403cea9ffc09a72b9efdacd57af3
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-24 14:56:11 +0900

    Deviseと重複したバリデーションの削除、先の修正箇所コメントアウト、バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }, presence: true
+  validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }, presence: true
   validates :birth_date, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
