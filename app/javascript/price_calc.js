93f8cfc6d8f0615ba63cb09952b72a466ed98f83
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-26 19:04:30 +0900

    手数料等の表示

---
+++ app/javascript/price_calc.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  const ProfitDom = document.getElementById("profit");
+  // 入力した金額から販売手数料を計算
+  addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+  // 出品価格から販売手数料を引
+  ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
+  })
+});
