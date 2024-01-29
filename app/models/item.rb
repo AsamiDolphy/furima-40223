23ab868aa47e69508c77fcf955fdd79147d3be37
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-29 16:03:52 +0900

    priceのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,6 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :image, :name, :info, :price, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, presence: true
   validates :category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :scheduled_delivery_id, numericality: { other_than: 0, message: "can't be blank" }, presence: true
 end
