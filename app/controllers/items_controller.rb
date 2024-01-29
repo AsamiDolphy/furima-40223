2e2caab479ee4e33ca2f466cf6d4b530c1961693
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-29 21:37:07 +0900

    差分の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show] 
 
   def new
     @item = Item.new
@@ -18,9 +18,9 @@ class ItemsController < ApplicationController
     @items = Item.all.order('created_at DESC')
   end
 
-  def show
-    @item = Item.find(params[:id])
-  end
+  def show 
+    @item = Item.find(params[:id]) 
+  end 
 
   private
 
