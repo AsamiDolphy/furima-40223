350559bcef9495a8b0d8bdf6518f9d1b2c0ac235
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-30 19:59:11 +0900

    destroyアクション リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,11 +39,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id
-      @item.destroy
-    else
-      redirect_to root_path
-    end
+    @item.destroy if @item.user_id == current_user.id
     redirect_to root_path
   end
 
