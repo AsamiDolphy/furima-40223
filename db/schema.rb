9e222c859d180d1230e4c4ab92dd2db151c80103
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-01-30 21:37:09 +0900

    ビューの表示

--- db/schema.rb
+++ db/schema.rb
@@ -55,8 +55,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_110945) do
   end
 
   create_table "orders", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "shipping_addresses", charset: "utf8", force: :cascade do |t|
@@ -89,4 +93,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_30_110945) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
