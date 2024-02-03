221dbc60aad0377e03e51a712e9c4e92bd263ca9
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-02-03 14:12:25 +0900

    テーブル修正、マイグレート済み

--- db/migrate/20240130161401_create_shipping_addresses.rb
+++ db/migrate/20240130161401_create_shipping_addresses.rb
@@ -5,7 +5,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[7.0]
       t.integer    :prefecture_id, null: false
       t.string     :city, null: false
       t.string     :address, null: false
-      t.string     :building, null: true
+      t.string     :building
       t.string     :phone_number, null: false
       t.references :order, null: false, foreign_key: true
       t.timestamps
