cb723c2f53c8642b7403500b511592256f35f2b7
Dolphy-A <154430437+AsamiDolphy@users.noreply.github.com>
2024-03-05 14:50:03 +0900

    jsを使わずに実装

--- app/javascript/image_switch.js
+++ app/javascript/image_switch.js
@@ -1,18 +1,4 @@
-// Use jQuery in noConflict mode
-var $j = jQuery.noConflict();
-
-// Wait for the DOM to be fully loaded
-$j(document).ready(function() {
-  // Add a click event listener to elements with the 'next-item-img' class
-  $j('.next-item-img').click(function() {
-    // Log a message to the console when the image is clicked
-    console.log("Clicked on next item image");
-
-    // Get the source attribute of the clicked image
-    var newSrc = $j(this).attr('src');
-    console.log("New source: " + newSrc);
-
-    // Set the source attribute of the '.item-box-img' element to the clicked image's source
-    $j('.item-box-img').attr('src', newSrc);
-  });
-});
+// $(document).on('click', '.next-item-img', function() {
+//   var newSrc = $(this).attr('src');
+//   $('.item-box-img').attr('src', newSrc);
+// });
